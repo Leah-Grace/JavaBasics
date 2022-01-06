66a74f763776048111f0f5c17d71e820ac1edb5b
Leah_Grace <leahgdg@hotmail.com>
2022-01-06 10:21:01 -0500

    createfullName() passed

---
+++ src/com/LeahGrace/Main.java
@@ -0,0 +1,13 @@
+package com.LeahGrace;
+
+public class Main {
+    static void createFullName(String firstName, String lastName){
+        System.out.println(firstName + " " + lastName);
+    }
+    public static void main(String[] args) {
+     createFullName("Gabriel", "Sherman");
+     createFullName("Marguerite", "Blair");
+        }
+
+    }
+
