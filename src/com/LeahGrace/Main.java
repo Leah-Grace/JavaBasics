576c41b7bbf86d02d727be1efea259a71621a5b8
Leah_Grace <leahgdg@hotmail.com>
2022-01-06 10:27:57 -0500

    "fullName printed from Main passed"

--- src/com/LeahGrace/Main.java
+++ src/com/LeahGrace/Main.java
@@ -1,13 +1,17 @@
 package com.LeahGrace;
 
 public class Main {
+
     static void createFullName(String firstName, String lastName){
         System.out.println(firstName + " " + lastName);
     }
     public static void main(String[] args) {
-     createFullName("Gabriel", "Sherman");
-     createFullName("Marguerite", "Blair");
+        String firstName = "Leah";
+        String lastName = "Grace";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+        createFullName("Gabriel", "Sherman");
+        createFullName("Marguerite", "Blair");
         }
-
     }
 
